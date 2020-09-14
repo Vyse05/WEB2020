bf85d734f747d47d898d37c826a373a6d639466a
Luka <kusic015@gmail.com>
2020-09-14 20:25:08 +0200

    datepicker

--- WebContent/Scripts/index.js
+++ WebContent/Scripts/index.js
@@ -1,141 +1,188 @@
-$(window).on(
-		'load',
-		function() {
+$(window)
+		.on(
+				'load',
+				function() {
 
-			var minMaxFilterEditor = function(cell, onRendered, success,
-					cancel, editorParams) {
+					var rezervacijaFormat = function(cell, formatterParams,
+							onRendered) { // plain
+						// text
+						// value
+						return "<label>Rezervisi</label>";
+					};
+					var komentarFormat = function(cell, formatterParams,
+							onRendered) { // plain
+						// text
+						// value
+						return "<label>Procitaj</label>";
+					};
+					komentarFormat
 
-				var end;
+					var minMaxFilterEditor = function(cell, onRendered,
+							success, cancel, editorParams) {
 
-				var container = document.createElement("span");
+						var end;
 
-				// create and style inputs
-				var start = document.createElement("input");
-				start.setAttribute("type", "number");
-				start.setAttribute("placeholder", "Min");
-				start.setAttribute("min", 0);
-				start.setAttribute("max", 100);
-				start.style.padding = "4px";
-				start.style.width = "50%";
-				start.style.boxSizing = "border-box";
+						var container = document.createElement("span");
 
-				start.value = cell.getValue();
+						// create and style inputs
+						var start = document.createElement("input");
+						start.setAttribute("type", "number");
+						start.setAttribute("placeholder", "Min");
+						start.setAttribute("min", 0);
+						start.setAttribute("max", 100);
+						start.style.padding = "4px";
+						start.style.width = "50%";
+						start.style.boxSizing = "border-box";
 
-				function buildValues() {
-					success({
-						start : start.value,
-						end : end.value,
-					});
-				}
+						start.value = cell.getValue();
 
-				function keypress(e) {
-					if (e.keyCode == 13) {
-						buildValues();
-					}
-
-					if (e.keyCode == 27) {
-						cancel();
-					}
-				}
-
-				end = start.cloneNode();
-				end.setAttribute("placeholder", "Max");
-
-				start.addEventListener("change", buildValues);
-				start.addEventListener("blur", buildValues);
-				start.addEventListener("keydown", keypress);
-
-				end.addEventListener("change", buildValues);
-				end.addEventListener("blur", buildValues);
-				end.addEventListener("keydown", keypress);
-
-				container.appendChild(start);
-				container.appendChild(end);
-
-				return container;
-			}
-
-			// custom max min filter function
-			function minMaxFilterFunction(headerValue, rowValue, rowData,
-					filterParams) {
-				// headerValue - the value of the header filter element
-				// rowValue - the value of the column in this row
-				// rowData - the data for the row being filtered
-				// filterParams - params object passed to the
-				// headerFilterFuncParams property
-
-				if (rowValue) {
-					if (headerValue.start != "") {
-						if (headerValue.end != "") {
-							return rowValue >= headerValue.start
-									&& rowValue <= headerValue.end;
-						} else {
-							return rowValue >= headerValue.start;
+						function buildValues() {
+							success({
+								start : start.value,
+								end : end.value,
+							});
 						}
-					} else {
-						if (headerValue.end != "") {
-							return rowValue <= headerValue.end;
+
+						function keypress(e) {
+							if (e.keyCode == 13) {
+								buildValues();
+							}
+
+							if (e.keyCode == 27) {
+								cancel();
+							}
 						}
+
+						end = start.cloneNode();
+						end.setAttribute("placeholder", "Max");
+
+						start.addEventListener("change", buildValues);
+						start.addEventListener("blur", buildValues);
+						start.addEventListener("keydown", keypress);
+
+						end.addEventListener("change", buildValues);
+						end.addEventListener("blur", buildValues);
+						end.addEventListener("keydown", keypress);
+
+						container.appendChild(start);
+						container.appendChild(end);
+
+						return container;
 					}
-				}
 
-				return true; // must return a boolean, true if it passes the
-				// filter.
-			}
+					// custom max min filter function
+					function minMaxFilterFunction(headerValue, rowValue,
+							rowData, filterParams) {
+						// headerValue - the value of the header filter element
+						// rowValue - the value of the column in this row
+						// rowData - the data for the row being filtered
+						// filterParams - params object passed to the
+						// headerFilterFuncParams property
 
-			var table = new Tabulator("#example-table", {
-				// data : tabledatabig,
-				ajaxURL : "../WebProj/rest/apartman/svi", // ajax URL
-				height : "311px",
-				layout : "fitColumns",
-				pagination : "local",
-				paginationSize : 5,
-				columns : [ {
-					title : "Lokacija",
-					field : "naseljenoMesto",
-					headerFilter : "input"
-				}, {
-					title : "Cena",
-					field : "cena",
-					width : 200,
-					sorter : "number",
-					hozAlign : "center",
-					headerFilter : minMaxFilterEditor,
-					headerFilterFunc : minMaxFilterFunction,
-					headerFilterLiveFilter : false
-				}, {
-					title : "Broj Soba",
-					field : "brojSoba",
-					sorter : "number",
-					hozAlign : "center",
-					width : 200,
-					headerFilter : "number",
-					headerFilterPlaceholder : "at least...",
-					headerFilterFunc : ">="
-				}, {
-					title : "Broj Osoba",
-					field : "brojGostiju",
-					sorter : "number",
-					hozAlign : "center",
-					width : 200,
-					headerFilter : "number",
-					headerFilterPlaceholder : "at least...",
-					headerFilterFunc : ">="
-				}, {
-					title : "Datum Dolaska",
-					field : "dob",
-					hozAlign : "center",
-					sorter : "date",
-					headerFilterFunc : ">="
-				}, {
-					title : "Datum Odlaska",
-					field : "dob2",
-					hozAlign : "center",
-					sorter : "date",
-					headerFilterFunc : ">="
-				},  ],
-				rowClick : function(e, row) {
-					alert("Row " + row.getIndex() + " Context Clicked!!!!")
-				},
-			});
-		});
+						if (rowValue) {
+							if (headerValue.start != "") {
+								if (headerValue.end != "") {
+									return rowValue >= headerValue.start
+											&& rowValue <= headerValue.end;
+								} else {
+									return rowValue >= headerValue.start;
+								}
+							} else {
+								if (headerValue.end != "") {
+									return rowValue <= headerValue.end;
+								}
+							}
+						}
+
+						return true; // must return a boolean, true if it
+						// passes the
+						// filter.
+					}
+
+					var table = new Tabulator(
+							"#example-table",
+							{
+								ajaxURL : "../WebProj/rest/apartman/svi", // ajax
+								// URL
+								height : "311px",
+								pagination : "local",
+								layout : "fitColumns",
+								paginationSize : 10,
+								columns : [
+										{
+											title : "Id",
+											field : "id",
+										},
+										{
+											title : "Lokacija",
+											field : "naseljenoMesto",
+											headerFilter : "input"
+										},
+										{
+											title : "Cena",
+											field : "cena",
+											sorter : "number",
+											hozAlign : "center",
+											headerFilter : minMaxFilterEditor,
+											headerFilterFunc : minMaxFilterFunction,
+											headerFilterLiveFilter : false
+										},
+										{
+											title : "Broj Soba",
+											field : "brojSoba",
+											sorter : "number",
+											hozAlign : "center",
+											headerFilter : "number",
+											headerFilterPlaceholder : "at least...",
+											headerFilterFunc : ">="
+										},
+										{
+											title : "Broj Osoba",
+											field : "brojGostiju",
+											sorter : "number",
+											hozAlign : "center",
+											headerFilter : "number",
+											headerFilterPlaceholder : "at least...",
+											headerFilterFunc : ">="
+										},
+										{
+											title : "Datum Dolaska",
+											field : "dob",
+											hozAlign : "center",
+											sorter : "date",
+											headerFilterFunc : ">="
+										},
+										{
+											title : "Datum Odlaska",
+											field : "dob2",
+											hozAlign : "center",
+											sorter : "date",
+											headerFilterFunc : ">="
+										},
+										{
+											formatter : rezervacijaFormat,
+											title : "Rezervacija",
+											hozAlign : "center",
+											cellClick : function(e, cell) {
+												alert("Redirefting to: "
+														+ "http://localhost:8080/WebProj/rest/rezervacija/nov/"
+															+ cell.getRow()
+															.getData().id);
+												window.location.href = "http://localhost:8080/WebProj/rest/rezervacija/nov/"
+														+ cell.getRow()
+																.getData().id;
+											}
+										},
+										{
+											formatter : komentarFormat,
+											title : "Komentari",
+											hozAlign : "center",
+											cellClick : function(e, cell) {
+												alert("Printing row data for: "
+														+ cell.getRow()
+																.getData().id)
+											}
+										}, ],
+							});
+
+				});
--- WebContent/Scripts/novRezervacija.js
+++ WebContent/Scripts/novRezervacija.js
@@ -1,66 +1,92 @@
-$(window).on('load', function(){
-	$.ajax({
-		type : 'GET',
-		url : '/WebProj/rest/korisnik/info',
-		dataType : "json",
-		success : function(data) {
-		},
-		error : function(XMLHttpRequest, textStatus, errorThrown) {
-			// Nije ulogovan
-			window.location.href = "/WebProj/rest/korisnik/login";			
-		}
-		});
-	
-	var url = window.location.pathname;
-	var apartmanId = url.substring(url.lastIndexOf('/') + 1);
-	
-	$.ajax({
-		type : 'GET',
-		url : '/WebProj/rest/apartman/'+apartmanId+'/info',
-		dataType : "json",
-		success : function(data) {
-			 $("#apartmanId").val(apartmanId);
-			 $("#apartman").text(data.ulica + " "+ data.broj + ", " +data.postanskiBroj + " " + data.naseljenoMesto);
-		},
-		error : function(XMLHttpRequest, textStatus, errorThrown) {
-			// Ne postoji apartman
-			$("#apartman").text("Apartman nije pronađen.");
-		}
-		});
-});
+$(window).on(
+		'load',
+		function() {
+			$.ajax({
+				type : 'GET',
+				url : '/WebProj/rest/korisnik/info',
+				dataType : "json",
+				success : function(data) {
+				},
+				error : function(XMLHttpRequest, textStatus, errorThrown) {
+					// Nije ulogovan
+					window.location.href = "/WebProj/rest/korisnik/login";
+				}
+			});
 
-$(document).on('submit','#forma',function(e){
-	$("#errorPocetniDatumRezervacije").hide();
-	$("#errorBrojNocenja").hide();
-	$("#errorSnimljeno").hide();
-	$("#snimljeno").hide();
-	e.preventDefault();
+			var url = window.location.pathname;
+			var apartmanId = url.substring(url.lastIndexOf('/') + 1);
 
-	var apartmanId = $("#apartmanId").val();
-	var pocetniDatumRezervacije = $("#pocetniDatumRezervacije").val();
-	var brojNocenja = $("#brojNocenja").val();
-	var poruka = $("#poruka").val();
-	
-	if (pocetniDatumRezervacije == "") {
-		$("#errorPocetniDatumRezervacije").show();
-	} else if (brojNocenja == "") {
-		$("#errorBrojNocenja").show();
-	}
-	else {
-		$.ajax({
-			type : 'POST',
-			url : "/WebProj/rest/rezervacija/nov",
-			contentType : 'application/json',
-			data : formToJSON(apartmanId, pocetniDatumRezervacije, brojNocenja, poruka),
-			success : function() {
-				$("#snimljeno").show();
-			},
-			error : function(XMLHttpRequest, textStatus, errorThrown) {
-				$("#errorSnimljeno").show();
+			$.ajax({
+				type : 'GET',
+				url : '/WebProj/rest/apartman/' + apartmanId + '/info',
+				dataType : "json",
+				success : function(data) {
+					$("#apartmanId").val(apartmanId);
+					$("#apartman").text(
+							data.ulica + " " + data.broj + ", "
+									+ data.postanskiBroj + " "
+									+ data.naseljenoMesto);
+				},
+				error : function(XMLHttpRequest, textStatus, errorThrown) {
+					// Ne postoji apartman
+					$("#apartman").text("Apartman nije pronađen.");
+				}
+			});
+		});
+
+$(document).on(
+		'submit',
+		'#forma',
+		function(e) {
+			$("#errorPocetniDatumRezervacije").hide();
+			$("#errorBrojNocenja").hide();
+			$("#errorSnimljeno").hide();
+			$("#snimljeno").hide();
+			e.preventDefault();
+
+			var apartmanId = $("#apartmanId").val();
+			var pocetniDatumRezervacije = $("#pocetniDatumRezervacije").val();
+			var brojNocenja = $("#brojNocenja").val();
+			var poruka = $("#poruka").val();
+
+			if (pocetniDatumRezervacije == "") {
+				$("#errorPocetniDatumRezervacije").show();
+			} else if (brojNocenja == "") {
+				$("#errorBrojNocenja").show();
+			} else {
+				$.ajax({
+					type : 'POST',
+					url : "/WebProj/rest/rezervacija/nov",
+					contentType : 'application/json',
+					data : formToJSON(apartmanId, pocetniDatumRezervacije,
+							brojNocenja, poruka),
+					success : function() {
+						$("#snimljeno").show();
+					},
+					error : function(XMLHttpRequest, textStatus, errorThrown) {
+						$("#errorSnimljeno").show();
+					}
+				});
 			}
-		});		
-	}
-	
+
+		});
+var disableDates = [ "9-11-2020", "14-11-2019", "15-11-2019", "16-9-2020", "27-12-2019" ];
+
+$(function() {
+
+	$('.datepicker').datepicker(
+			{
+				startDate : new Date(),
+				beforeShowDay : function(date) {
+					dmy = date.getDate() + "-" + (date.getMonth() + 1) + "-"
+							+ date.getFullYear();
+					if (disableDates.indexOf(dmy) != -1) {
+						return false;
+					} else {
+						return true;
+					}
+				}
+			});
 });
 
 function formToJSON(apartmanId, pocetniDatumRezervacije, brojNocenja, poruka) {
